5d295fe77167944ec396add01bcb1f86d1127d04
MarcosRolando <marcosrolando.mr@gmail.com>
2020-04-27 12:15:09 -0300

    Mejoro modularizacion del protocolo del cliente, no esta terminado

--- ClientCommand.c
+++ ClientCommand.c
@@ -8,15 +8,18 @@
 #include "stringExtra.h"
 #include <stdio.h>
 
-#define HEADER_BASE_LENGTH 48 //tamanio minimo del header en bytes
+#define HEADER_BASE_LENGTH 52 //tamanio minimo del header en bytes
 
 static void _deleteParametersSeparator(ClientCommand* this) {
-    for (int i = 0; i < strlen(this->parameters); ++i) {
-        if (this->parameters[i] == ',') this->parameters[i] = '\0';
+    for (int i = 0; i < (strlen(this->parameters) + 1); ++i) { //quiero contar el \0 xq es otro parametro mas
+        if (this->parameters[i] == ',') {
+            this->parameters[i] = '\0';
+        }
+        if (this->parameters[i] == '\0') this->parameterAmount++;
     }
 }
 
-static uint32_t _calculateHeaderLength(ClientCommand* this) {
+static void _calculateCommandLength(ClientCommand* this) {
     uint32_t destLength = this->dLength + 1;//cuento el \0
     destLength +=  (8 - destLength % 8); //cuento el padding
     uint32_t pathLength = this->pathLength + 1;//cuento el \0
@@ -25,27 +28,45 @@ static uint32_t _calculateHeaderLength(ClientCommand* this) {
     interLength += (8 - interLength % 8);
     uint32_t methLength = this->mLength + 1;//cuento el \0
     methLength += (8 - methLength % 8);
-    return (HEADER_BASE_LENGTH + destLength + pathLength + interLength
-                                                                + methLength);
+    this->commandLenght = destLength + pathLength + interLength + methLength +
+                                                        this->parameterAmount; //esto seria el arrayLength en el PDF
+}
+
+static uint32_t _calculateHeaderLength(ClientCommand* this) {
+    _calculateCommandLength(this);
+    uint32_t bufferLength = HEADER_BASE_LENGTH + this->commandLenght;
+    bufferLength += (8 - bufferLength % 8); //el buffer debe ser multiplo de 8
+    return bufferLength;
+}
+
+static void _loadHeaderSettings(ClientCommand* this, char* header,
+                                uint32_t messageID, uint32_t* bytesWritten) {
+    char endiannes = 'l', messageType = 0x01, flag = 0x00, pVersion = 0x01;
+    uint32_t bodyLength = htole32(this->paraLength + 5); //4 bytes indicando la longitud + el byte del \0
+    messageID = htole32(messageID);
+    *bytesWritten = snprintf(header, 4, "%c%c%c%c", //todo ACA NO SE SI VA 4 O 5 POR EL NULL CHARACTER, VER
+                                        endiannes, messageType, flag, pVersion);
+    memcpy(header + *bytesWritten, &bodyLength, sizeof(uint32_t));
+    bytesWritten += sizeof(uint32_t);
+    memcpy(header + *bytesWritten, &messageID, sizeof(uint32_t));
+    *bytesWritten += sizeof(uint32_t);
+}
+
+static void _loadHeaderCommands(ClientCommand* this, char* header,
+                                                    uint32_t* bytesWritten) {
+
 }
 
 //todo: tengo que avanzar esta funcion para escribir el protocolo
 //por ahora me limito a armar el header
 void clientCommandSetMessage(ClientCommand* this, uint32_t messageID) {
-    char messageType = 0x01, flag = 0x00, pVersion = 0x01;
     uint32_t headerLength = _calculateHeaderLength(this);
-    uint32_t bodyLength = htole32(this->paraLength); //no se cuenta el \0
-    headerLength = htole32(headerLength);
-    messageID = htole32(messageID);
-    char* header = malloc(headerLength*sizeof(char));
-    uint32_t bytesWritten;
-    bytesWritten = snprintf(header, headerLength, "l%c%c%c", messageType,
-                                                                flag, pVersion);
-    memcpy(header + bytesWritten, &bodyLength, sizeof(uint32_t));
-    bytesWritten += sizeof(uint32_t);
-    memcpy(header + bytesWritten, &messageID, sizeof(uint32_t));
-    bytesWritten += sizeof(uint32_t);
-    memcpy(header + bytesWritten, &headerLength, sizeof(uint32_t));
+    char* header = malloc(headerLength * sizeof(char));
+    uint32_t bytesWritten = 0;
+    _loadHeaderSettings(this, header, messageID, &bytesWritten); //cargo la parte que no es el array of struct, es decir, primeros 4 bytes y 2 uints
+    _loadHeaderCommands(this, header, &bytesWritten);
+
+    memcpy(header + bytesWritten, &this->headerLength , sizeof(uint32_t));
     free(header);
 }
 
@@ -55,6 +76,8 @@ void clientCommandCreate(ClientCommand* this) {
     this->interface = NULL;
     this->method = NULL;
     this->parameters = NULL;
+    this->parameterAmount = 0;
+    this->commandLenght = 0;
 }
 
 void clientCommandDestroy(ClientCommand* this) {
--- ClientCommand.h
+++ ClientCommand.h
@@ -10,6 +10,9 @@
 typedef struct ClientCommand {
     char *destiny, *path, *interface, *method, *parameters;
     uint32_t dLength, pathLength, iLength, mLength, paraLength; //no cuentan el \0
+    uint32_t parameterAmount;
+    uint32_t commandLenght; //esto solo cuenta el largo de los comandos (\0 incluido)
+                        // sin tener en cuenta el padding de los parametros por cada s
 } ClientCommand;
 
 void clientCommandCreate(ClientCommand* this);
